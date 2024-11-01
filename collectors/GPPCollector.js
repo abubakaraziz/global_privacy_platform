38034b212a158708f92becf17d5913f7c136cb1c
Jawamegamind <25100094@lums.edu.pk>
2024-11-01 20:34:28 +0500

    Refactorizing code

--- collectors/GPPCollector.js
+++ collectors/GPPCollector.js
@@ -1,6 +1,12 @@
 /* eslint-disable no-await-in-loop */
 const BaseCollector = require('./BaseCollector');
 const createDeferred = require('../helpers/deferred');
+// Loading in the helper uspPing function
+const uspPing = require('../helpers/uspPing');
+const gppPing = require('../helpers/gppPing');
+const callGPPhasSections = require('../helpers/gppHasSections');
+const callGPPgetSections = require('../helpers/gppGetSections');
+const callGPPgetField = require('../helpers/gppGetField');
 
 /**
  * @typedef {Object} ScanResult
@@ -8,6 +14,7 @@ const createDeferred = require('../helpers/deferred');
  * @property {{ api: any; hasSection: any; }[]} hasSections
  * @property {{ api: any; getSection: any; }[]} getSections
  * @property {any[]} getField
+ * @property {string[]} uspString
  */
 
 /**
@@ -34,176 +41,10 @@ class GPPCollector extends BaseCollector {
             gppObjects: [],
             hasSections: [],
             getSections: [],
-            getField: []
+            getField: [],
+            uspString: []
         };
     }
-
-    /**
-     * @param {import('puppeteer').Page} page - The Puppeteer page instance.
-     */
-    /* eslint-disable no-undef */
-    async callGPPPing(page) {
-        try {
-            const gppObject = await page.evaluate(() => new Promise(resolve => {
-                    // Check if __gpp function exists on the window object
-                    // @ts-ignore
-                if (typeof window.__gpp !== 'function') {
-                    resolve(null); // Resolve with null if __gpp doesn't exist
-                    return;
-                }
-                    // Call the __gpp function if it exists
-                    // @ts-ignore
-                window.__gpp('ping', (gppData, success) => {
-                    if (success) {
-                        resolve(gppData);
-                    } else {
-                        resolve(null);
-                    }
-                });
-            }));
-            if (gppObject) {
-                console.log('GPP object retrieved:', gppObject);
-            } else {
-                console.log('No GPP object retrieved.');
-            }
-            return gppObject; // Return the retrieved object
-        } catch (error) {
-            console.error('Error calling GPP function:', error);
-            return null;
-        }
-    }
-    /* eslint-disable no-undef */
-    
-
-    /**
-     * @param {import('puppeteer').Page} page - The Puppeteer page instance.
-     * @param {any} gppObject - The GPP object to use for the hasSection calls.
-     */
-    async callGPPhasSections(page, gppObject) {
-        try {
-            // Extract supported APIs from the gppObject
-            const supportedAPIs = gppObject.supportedAPIs;
-            const hasSections = [];
-
-            // Loop through each supported API and call hasSection
-            for (const api of supportedAPIs) {
-                // Extract the part after the colon (e.g., 'tcfcav1' from '5:tcfcav1')
-                const apiIdentifier = api.split(':')[1];
-
-                // Check if we have a valid apiIdentifier
-                if (apiIdentifier) {
-                    const hasSection = await page.evaluate(apiId => new Promise(resolve => {
-                            // Check if __gpp function exists on the window object
-                            // @ts-ignore
-                        if (typeof window.__gpp !== 'function') {
-                            resolve(null); // Resolve with null if __gpp doesn't exist
-                            return;
-                        }
-                            // Call the __gpp function with the apiId
-                            // @ts-ignore
-                        window.__gpp('hasSection', (/** @type {Boolean} */ data, /** @type {Boolean} */ success) => {
-                            if (success) {
-                                resolve(data);
-                            } else {
-                                resolve(null);
-                            }
-                        }, apiId); // Pass the dynamic apiId (e.g., 'tcfcav1')
-                    }), apiIdentifier);
-
-                    // Add the result (true/false/null) to the hasSections array
-                    hasSections.push({api: apiIdentifier, hasSection});
-                }
-            }
-            return hasSections; // Return the array of sections
-        } catch (error) {
-            console.error('Error checking for sections:', error);
-            return null;
-        }
-        
-    }
-
-    /**
-     * @param {import('puppeteer').Page} page - The Puppeteer page instance.
-     * @param {any} gppObject - The GPP object to use for the getSection calls.
-     */
-    async callGPPgetSections(page, gppObject) {
-        try {
-            // Extract supported APIs from the gppObject
-            const supportedAPIs = gppObject.supportedAPIs;
-            const getSections = [];
-
-            // Loop through each supported API and call getSection
-            for (const api of supportedAPIs) {
-                // Extract the part after the colon (e.g., 'tcfcav1' from '5:tcfcav1')
-                const apiIdentifier = api.split(':')[1];
-
-                // Check if we have a valid apiIdentifier
-                if (apiIdentifier) {
-                    const getSection = await page.evaluate(apiId => new Promise(resolve => {
-                            // Check if __gpp function exists on the window object
-                            // @ts-ignore
-                        if (typeof window.__gpp !== 'function') {
-                            resolve(null); // Resolve with null if __gpp doesn't exist
-                            return;
-                        }
-                            // Call the __gpp function with the apiId
-                            // @ts-ignore
-                        window.__gpp('getSection', (/** @type {Boolean} */ data, /** @type {Boolean} */ success) => {
-                            if (success) {
-                                resolve(data);
-                            } else {
-                                resolve(null);
-                            }
-                        }, apiId); // Pass the dynamic apiId (e.g., 'tcfcav1')
-                    }), apiIdentifier);
-
-                    // Add the result (true/false/null) to the getSections array
-                    getSections.push({api: apiIdentifier, getSection});
-                }
-            }
-            return getSections; // Return the array of sections
-        } catch (error) {
-            console.error('Error checking for sections:', error);
-            return null;
-        }
-    }
-
-    /**
-     * @param {import('puppeteer').Page} page - The Puppeteer page instance.
-     * @param {string} fieldName - The field name to retrieve from the GPP object.
-     */
-    async callGPPgetField(page, fieldName) {
-        try {
-            const getField = await page.evaluate(field => new Promise(resolve => {
-                    // Check if __gpp function exists on the window object
-                    // @ts-ignore
-                if (typeof window.__gpp !== 'function') {
-                    resolve(null); // Resolve with null if __gpp doesn't exist
-                    return;
-                }
-                    // Call the __gpp function if it exists
-                    // @ts-ignore
-                window.__gpp('getField', (gppData, success) => {
-                    if (success) {
-                        resolve(gppData);
-                    } else {
-                        resolve(null);
-                    }
-                }, field);
-            }), fieldName);  // Pass fieldName as an argument here
-            
-            if (getField) {
-                console.log('GPP field retrieved:', getField);
-            } else {
-                console.log('No GPP field retrieved.');
-            }
-            return getField; // Return the retrieved object
-        } catch (error) {
-            console.error('Error calling GPP function:', error);
-            return null;
-        }
-    }
-
 
     async postLoad() {
         /**
@@ -226,7 +67,7 @@ class GPPCollector extends BaseCollector {
           
             // let gppObject  = [""]
             console.log('Attempting to retrieve GPP objects...');
-            const gppObject = await this.callGPPPing(page);
+            const gppObject = await gppPing(page);
 
             if (gppObject) {
                 gppObjects.push(gppObject);
@@ -236,7 +77,7 @@ class GPPCollector extends BaseCollector {
             }
 
             console.log('Attempting to check for sections...');
-            const hasSection = await this.callGPPhasSections(page, gppObject);
+            const hasSection = await callGPPhasSections(page, gppObject);
 
             if (hasSection) {
                 hasSections.push(...hasSection);
@@ -246,7 +87,8 @@ class GPPCollector extends BaseCollector {
             }
 
             console.log('Attempting to get sections...');
-            const getSection = await this.callGPPgetSections(page, gppObject);
+            // const getSection = await this.callGPPgetSections(page, gppObject);
+            const getSection = await callGPPgetSections(page, gppObject);
 
             if (getSection) {
                 getSections.push(...getSection);
@@ -257,7 +99,8 @@ class GPPCollector extends BaseCollector {
 
             console.log('Attempting to get field...');
             // const getField = await this.callGPPgetField(page, "tcfcav1.LastUpdated");
-            const getField = await this.callGPPgetField(page, "tcfcav1.CmpId");
+            // const getField = await this.callGPPgetField(page, "tcfcav1.CmpId");
+            const getField = await callGPPgetField(page, "tcfcav1.CmpId");
 
             if (getField) {
                 getFields.push(getField);
@@ -265,13 +108,24 @@ class GPPCollector extends BaseCollector {
             } else {
                 console.log('No field found.');
             }
+
+            console.log('Attempting to retrieve USP string...');
+            const uspString = await uspPing(page);
+
+            if (uspString) {
+                this.scanResult.uspString.push(uspString);
+                console.log('USP string retrieved:', uspString);
+            } else {
+                console.log('No USP string retrieved.');
+            }
         }
         this.pendingScan.resolve();
         this.scanResult = {
             gppObjects,
             hasSections,
             getSections,
-            getField: getFields
+            getField: getFields,
+            uspString: this.scanResult.uspString
         };
         console.log('Scan result:', this.scanResult);
     }
---
+++ helpers/gppGetField.js
@@ -0,0 +1,38 @@
+/**
+ * @param {import('puppeteer').Page} page - The Puppeteer page instance.
+ * @param {string} fieldName - The field name to retrieve from the GPP object.
+ */
+/* eslint-disable no-undef */
+const callGPPgetField = async(page, fieldName) => {
+    try {
+        const getField = await page.evaluate(field => new Promise(resolve => {
+                // Check if __gpp function exists on the window object
+                // @ts-ignore
+            if (typeof window.__gpp !== 'function') {
+                resolve(null); // Resolve with null if __gpp doesn't exist
+                return;
+            }
+                // Call the __gpp function if it exists
+                // @ts-ignore
+            window.__gpp('getField', (gppData, success) => {
+                if (success) {
+                    resolve(gppData);
+                } else {
+                    resolve(null);
+                }
+            }, field);
+        }), fieldName);  // Pass fieldName as an argument here
+        
+        if (getField) {
+            console.log('GPP field retrieved:', getField);
+        } else {
+            console.log('No GPP field retrieved.');
+        }
+        return getField; // Return the retrieved object
+    } catch (error) {
+        console.error('Error calling GPP function:', error);
+        return null;
+    }
+};
+
+module.exports = callGPPgetField;
