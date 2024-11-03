e4af93d53b335f4a321e5efee136f96aeb3db3a8
raahemn <25100091@lums.edu.pk>
2024-11-03 17:51:18 +0500

    CMP Detection and Optout for Onetrust and Didomi in GPP Collector

--- collectors/GPPCollector.js
+++ collectors/GPPCollector.js
@@ -1,18 +1,20 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-await-in-loop */
-const BaseCollector = require('./BaseCollector');
-const createDeferred = require('../helpers/deferred');
+const BaseCollector = require("./BaseCollector");
+const createDeferred = require("../helpers/deferred");
 // Loading in the helper uspPing function
-const uspPing = require('../helpers/uspPing');
-const gppPing = require('../helpers/gppPing');
-const callGPPhasSections = require('../helpers/gppHasSections');
-const callGPPgetSections = require('../helpers/gppGetSections');
-const callGPPgetField = require('../helpers/gppGetField');
-const tcfPing = require('../helpers/tcfPing');
-const tcfEventListener = require('../helpers/tcfEventListener');
+const uspPing = require("../helpers/uspPing");
+const gppPing = require("../helpers/gppPing");
+const callGPPhasSections = require("../helpers/gppHasSections");
+const callGPPgetSections = require("../helpers/gppGetSections");
+const callGPPgetField = require("../helpers/gppGetField");
+const tcfPing = require("../helpers/tcfPing");
+const tcfEventListener = require("../helpers/tcfEventListener");
+const CMPCollector = require("./CMPCollector");
 
 /**
  * @typedef {Object} ScanResult
+ * @property {any[]} cmpData
  * @property {string[]} gppObjects
  * @property {{ api: any; hasSection: any; }[]} hasSections
  * @property {{ api: any; getSection: any; }[]} getSections
@@ -23,7 +25,7 @@ const tcfEventListener = require('../helpers/tcfEventListener');
  */
 
 /**
- * Scrolls to the bottom of the page in increments.
+ *
  *
  * @param {import('puppeteer').Page} page - The Puppeteer page instance.
  * @returns {Promise<void>} - A promise that resolves when the page has been scrolled to the bottom.
@@ -31,7 +33,7 @@ const tcfEventListener = require('../helpers/tcfEventListener');
 
 class GPPCollector extends BaseCollector {
     id() {
-        return 'gpp';
+        return "gpp";
     }
 
     /**
@@ -43,14 +45,27 @@ class GPPCollector extends BaseCollector {
         this.pendingScan = createDeferred();
         /** @type {ScanResult} */
         this.scanResult = {
+            cmpData: [],
             gppObjects: [],
             hasSections: [],
             getSections: [],
             getField: [],
             uspString: [],
             tcfString: [],
-            tcfEventListenerData: []
+            tcfEventListenerData: [],
         };
+
+        //intialize the CMP collector as well
+        this.cmpCollector = new CMPCollector();
+        this.cmpCollector.init(options);
+    }
+
+    /**
+     * @param {{cdpClient: import('puppeteer').CDPSession, url: string, type: import('./TargetCollector').TargetType}} targetInfo
+     */
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async addTarget(targetInfo) {
+        await this.cmpCollector.addTarget(targetInfo);
     }
 
     async postLoad() {
@@ -61,13 +76,64 @@ class GPPCollector extends BaseCollector {
         const hasSections = [];
         const getSections = [];
         const getFields = [];
-    
-        const pages = await this.context.pages();
 
+        /**
+         * @type {any[]}
+         */
+        let cmpData = [];
+
+        const pages = await this.context.pages();
 
         if (pages.length > 0) {
             const page = pages[0];
-            
+
+            try {
+                //call the CMP collector postLoad
+                await this.cmpCollector.postLoad();
+
+                //get the data from the CMP collector
+                cmpData = await this.cmpCollector.getData();
+
+                console.log("CMP data retrieved by GPP collector:", cmpData);
+
+                if (cmpData) {
+                    //now that we have the CMP data, we can check if it belongs to one of our target CMPs
+                    if (cmpData[0].name === "com_didomi.io") {
+                        //if the CMP is Didomi, we follow the Didomi specific logic
+                        console.log("Didomi CMP detected");
+
+                        await page.evaluate(() => {
+                            // Checking first if the didomi consent prompt is visible
+                            console.log("checking for didomi notice");
+
+                            // @ts-ignore
+                            if (window.Didomi.notice.isVisible()) {
+                                console.log("didomi banner visible, opting out");
+                                // Trigger the function for opting out
+                                // @ts-ignore
+                                window.Didomi.setUserDisagreeToAll();
+                            }
+                        });
+                    } else if (cmpData[0].name === "Onetrust") {
+                        //if the CMP is OneTrust, we follow the OneTrust specific logic - reference for this can be found on: https://developer.onetrust.com/onetrust/docs/javascript-api
+                        console.log("OneTrust CMP detected");
+
+                        await page.evaluate(() => {
+                            console.log("Opting out of OneTrust");
+
+                            // @ts-ignore
+                            // eslint-disable-next-line new-cap
+                            window.OneTrust.RejectAll();
+                        });
+                    }
+                }
+            } catch (error) {
+                console.error(
+                    "Error while processing CMP data or opting out:",
+                    error
+                );
+            }
+
             // Scroll to the bottom of the page to load all the content
             // await page.waitForTimeout(2000);
             // await scrollToBottom(page);
@@ -76,97 +142,89 @@ class GPPCollector extends BaseCollector {
             // @ts-ignore
             const updateScanResultWithEventData = tcData => {
                 this.scanResult.tcfEventListenerData.push(tcData);
-                console.log('TCF event data added to scanResult:', tcData);
+                // console.log('TCF event data added to scanResult:', tcData);
             };
 
             // Add event listener to listen for TCF events
             await tcfEventListener(page, updateScanResultWithEventData);
 
-            console.log('Attempting to retrieve GPP objects...');
+            console.log("Attempting to retrieve GPP objects...");
             const gppObject = await gppPing(page);
 
             if (gppObject) {
                 gppObjects.push(gppObject);
-                console.log('GPP object retrieved:', gppObject);
+                // console.log('GPP object retrieved:', gppObject);
             } else {
-                console.log('No GPP object retrieved.');
+                console.log("No GPP object retrieved.");
             }
 
-            console.log('Attempting to check for sections...');
+            console.log("Attempting to check for sections...");
             const hasSection = await callGPPhasSections(page, gppObject);
 
             if (hasSection) {
                 hasSections.push(...hasSection);
-                console.log('Sections found:', hasSection);
+                // console.log('Sections found:', hasSection);
             } else {
-                console.log('No sections found.');
+                console.log("No sections found.");
             }
 
-            console.log('Attempting to get sections...');
+            console.log("Attempting to get sections...");
             // const getSection = await this.callGPPgetSections(page, gppObject);
             const getSection = await callGPPgetSections(page, gppObject);
 
             if (getSection) {
                 getSections.push(...getSection);
-                console.log('Sections found:', getSection);
+                // console.log('Sections found:', getSection);
             } else {
-                console.log('No sections found.');
+                console.log("No sections found.");
             }
 
-            console.log('Attempting to get field...');
+            console.log("Attempting to get field...");
             // const getField = await this.callGPPgetField(page, "tcfcav1.LastUpdated");
             // const getField = await this.callGPPgetField(page, "tcfcav1.CmpId");
             const getField = await callGPPgetField(page, "tcfcav1.CmpId");
 
             if (getField) {
                 getFields.push(getField);
-                console.log('Field found:', getField);
+                // console.log('Field found:', getField);
             } else {
-                console.log('No field found.');
+                console.log("No field found.");
             }
 
-            console.log('Attempting to retrieve USP string...');
+            console.log("Attempting to retrieve USP string...");
             const uspString = await uspPing(page);
 
             if (uspString) {
                 this.scanResult.uspString.push(uspString);
-                console.log('USP string retrieved:', uspString);
+                // console.log('USP string retrieved:', uspString);
             } else {
-                console.log('No USP string retrieved.');
+                console.log("No USP string retrieved.");
             }
 
-            console.log('Attempting to retrieve TCF string...');
+            console.log("Attempting to retrieve TCF string...");
             const tcfString = await tcfPing(page);
 
             if (tcfString) {
                 this.scanResult.tcfString.push(tcfString);
-                console.log('TCF string retrieved:', tcfString);
-            }else {
-                console.log('No TCF string retrieved.');
+                // console.log('TCF string retrieved:', tcfString);
+            } else {
+                console.log("No TCF string retrieved.");
             }
 
-            // Triggering the accept all option on the cookie banner on the website
-            await page.evaluate(() => {
-                // Checking first if the didomi consent prompt is visible
-                // @ts-ignore
-                if (window.Didomi.notice.isVisible()) {
-                    // Trigger the function for opting out
-                    // @ts-ignore
-                    window.Didomi.setUserAgreeToAll();
-                }
-            });
+            console.log("CMP data retrieved by GPP collector:", cmpData);
         }
         this.pendingScan.resolve();
         this.scanResult = {
+            cmpData,
             gppObjects,
             hasSections,
             getSections,
             getField: getFields,
             uspString: this.scanResult.uspString,
             tcfString: this.scanResult.tcfString,
-            tcfEventListenerData: this.scanResult.tcfEventListenerData
+            tcfEventListenerData: this.scanResult.tcfEventListenerData,
         };
-        console.log('Scan result:', this.scanResult);
+        // console.log('Scan result:', this.scanResult);
     }
 
     getData() {
@@ -177,9 +235,9 @@ class GPPCollector extends BaseCollector {
         // await options.page.evaluate(() => {
         //     window.scrollTo(0, 0);
         // });
-        
+
         return this.scanResult;
     }
- }
+}
 
 module.exports = GPPCollector;
