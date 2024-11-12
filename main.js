2ff38842bdfc788536d1d0595e5468ed61f414f1
Jawamegamind <25100094@lums.edu.pk>
2024-11-12 21:47:23 +0500

    Adding LinkCollector for a 10k crawl

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const TraceCollector = require('./collectors/TraceCollector');
 const ScreenshotCollector = require('./collectors/ScreenshotCollector');
 const CMPCollector = require('./collectors/CMPCollector');
 const GPPCollector = require('./collectors/GPPCollector');
+const LinkCollector = require('./collectors/LinkCollector');
 
 // reexport main pieces of code so that they can be easily imported when this project is used as a dependency
 // e.g. `const {crawlerConductor} = require('3p-crawler');`
@@ -23,5 +24,6 @@ module.exports = {
     TraceCollector,
     ScreenshotCollector,
     CMPCollector,
-    GPPCollector
+    GPPCollector,
+    LinkCollector,
 };
