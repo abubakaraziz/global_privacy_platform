c68182fa28507f048e3dea199a0a006e56108064
raahemn <25100091@lums.edu.pk>
2025-01-17 03:01:24 +0500

    GPP commands completed

--- helpers/injectGpp.js
+++ helpers/injectGpp.js
@@ -1,6 +1,12 @@
 const overWriteGPP = `
     Object.defineProperty(window, "__gpp", {
-        value: function(command, callback) {
+        value: function(command, callback, parameter) {
+
+            //Maintain a counter for the listenerId to keep them unique
+            let registeredListeners = {}; // Stores registered listeners with their listenerId
+            let nextListenerId = 1; // Incremental ID for new listeners
+
+
             //Define the GPP data object
             let gppData = {
                 "gppVersion": "1.1",
@@ -68,9 +74,54 @@ const overWriteGPP = `
             let stack = new Error().stack;
             console.log("web_gpp_called: ", command, ", Arguments: ", arguments, ", full stack: ", stack);
 
-            //Callback with the GPP data
-            if (callback) {
-                callback(gppData, true); 
+            //Execute the Callback function according to the relevant command
+            if (command === "ping") {
+                callback(gppData, true);                    //Pass the PingReturn object with the success status
+            
+            } else if (command === "hasSection") {
+                const hasSection = gppData.supportedAPIs.some((section) => section.includes(parameter));
+                callback(hasSection, true);                 //Pass the hasSection boolean with the success status
+
+            } else if (command === "getSection") {
+                const section = gppData.parsedSections[parameter];
+                if (section) {
+                    callback(section, true); //Pass the section
+                } else {
+                    callback(null, false); //Pass the null with the failure status
+                }
+            } else if (command === "getField") {
+                if (parameter) {
+                    const [section, field] = parameter.split(".");
+                    const sectionData = gppData.parsedSections[section];
+
+                    if (sectionData && sectionData[field]) {
+                        callback(sectionData[field], true);         //Pass the field with the success status
+                    } else {
+                        callback(null, false);                      //Pass the null with the failure status
+                    }
+                } else {
+                    callback(null, false); 
+                }
+            } else if (command === "addEventListener") {
+                const listenerId = nextListenerId++;
+                registeredListeners[listenerId] = {
+                    eventName: "listenerRegistered",
+                    listenerId: listenerId,
+                    data: {listenerRegistered: true},
+                    pingData: gppData
+                };
+                callback(registeredListeners[listenerId], true); 
+            } else if (command === "removeEventListener") {
+
+                const listenerId = parameter;
+                if (registeredListeners[listenerId]) {
+                    delete registeredListeners[listenerId];
+                    callback(true, true); 
+                } else {
+                    callback(false, true); 
+                }
+            } else {
+                callback(null, false); // Pass the null with the failure status since the command was invalid
             }
 
             return 100;
