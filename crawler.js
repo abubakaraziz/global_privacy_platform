3d993a6516719a5091611ec4f56c767aec635e9d
Muhammad Abu Bakar Aziz <aziz313f@gmail.com>
2025-04-20 16:46:26 -0400

    Prevent Stopping Scripts after timeout

--- crawler.js
+++ crawler.js
@@ -263,7 +263,11 @@ async function getSiteData(context, url, {
             for (let target of targets) {
                 if (target.type === 'page') {
                     // eslint-disable-next-line no-await-in-loop
-                    await target.cdpClient.send('Page.stopLoading');
+                    /*Comment out the following linem, because even after page is still loading after timeout, we do not want to
+                    prevent external scripts from loading, because we want to collect all data from the page.
+                    */
+                    //await target.cdpClient.send('Page.stopLoading');
+                    
                 }
             }
             timeout = true;
